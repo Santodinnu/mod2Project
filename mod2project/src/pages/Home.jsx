d59f71eda3b8a0c3098a07ac2145382469db681f
santodinnu <shantoshi@gmail.com>
2024-01-04 12:00:29 -0500

    in the home page called the random and veggie functions

---
+++ mod2project/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Random from "../components/main/Random";
+import Veggie from "../components/main/Veggie";
+
+function Home() {
+  return (
+    <>
+      <div>
+        <Random />
+        <Veggie />
+      </div>
+    </>
+  );
+}
+
+export default Home;
