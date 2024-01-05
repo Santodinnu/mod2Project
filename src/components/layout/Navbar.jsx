6eee1102d2621d0d4075fcdb4684e2add9a0bf2e
santodinnu <shantoshi@gmail.com>
2024-01-05 12:31:46 -0500

    included .env in .gitignore

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -37,7 +37,6 @@ function Navbar() {
         <section className="container">
           <div className="flex items-center justify-between gap-4 py-4">
             <Link to="/">
-              {/* <h1 className="text-3xl font-bold cursor-pointer">CookRecipes</h1> */}
               <img 
                             src="https://th.bing.com/th/id/OIG.cz2Fwoqa4ZWomzc6q0SH?pid=ImgGn"
                             className="w-64 h-48 rounded-full"
